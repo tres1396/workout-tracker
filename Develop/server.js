13dc5455a90c566926b3a9c16213278e0c7d0227
Tre Scott <tres1396@gmail.com>
2021-05-23 15:52:26 -0400

    had to revert in commits lost the server. Added it back

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+const expres = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
