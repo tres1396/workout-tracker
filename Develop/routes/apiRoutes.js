be51e0f273287f34858c8d56e9d81b7deae9c4d0
Tre Scott <tres1396@gmail.com>
2021-05-23 15:46:32 -0400

    adding api route

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+
+const Workout = require("../models/workout");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err.message);
+    });
+});
+
+router.post("/api/workouts", { body }, (res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err.message);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err.message);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    { $sort: { day: -1 } },
+    { $limit: 7 },
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    { $sort: { day: 1 } },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err.message);
+    });
+});
+
+module.exports = router;
